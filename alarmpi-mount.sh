ca30dcbab13942ce7ab8539eafe8352aa7578400
Attila BODY <attila.body@gmail.com>
2022-03-17 11:56:38 +0100

    Using sudo in the rest of the scripts too

--- alarmpi-mount.sh
+++ alarmpi-mount.sh
@@ -10,15 +10,15 @@ if [ ! -d "$DST" ]; then mkdir "$DST"; fi
 if [ -b "$IMG" ]; then
   parts=( "$IMG"1 "$IMG"2 )
 else
-  parts=( $(kpartx -av "$IMG"|cut -f3 -d ' ') )
+  parts=( $(sudo kpartx -av "$IMG"|cut -f3 -d ' ') )
   parts[0]=/dev/mapper/${parts[0]}
   parts[1]=/dev/mapper/${parts[1]}
 fi
 
-mount ${parts[1]} "$DST" -ocompress=zstd:15,subvol=@arch_root
-mount ${parts[0]} "$DST"/boot
-mount --bind "$CACHE" "$DST/var/cache/pacman"
-for d in dev run proc sys; do mount --bind /$d "$DST"/$d; done
-[ -d /run/systemd/resolve/ ] || mkdir -p /run/systemd/resolve
-[ -f /run/systemd/resolve/resolv.conf ] || cp -L /etc/resolv.conf /run/systemd/resolve/
+sudo mount ${parts[1]} "$DST" -ocompress=zstd:15,subvol=@arch_root
+sudo mount ${parts[0]} "$DST"/boot
+sudo mount --bind "$CACHE" "$DST/var/cache/pacman"
+for d in dev run proc sys; do sudo mount --bind /$d "$DST"/$d; done
+[ -d /run/systemd/resolve/ ] || sudo mkdir -p /run/systemd/resolve
+[ -f /run/systemd/resolve/resolv.conf ] || sudo cp -L /etc/resolv.conf /run/systemd/resolve/
 
--- alarmpi-umount.sh
+++ alarmpi-umount.sh
@@ -4,6 +4,6 @@ set -ex
 IMG="${1:-alarmpi.img}"
 DST="${1:-alarmpi}"
 
-umount -R "$DST"
-kpartx -d "$IMG"
+sudo umount -R "$DST"
+sudo kpartx -d "$IMG"
 
